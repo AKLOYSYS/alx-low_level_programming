c4ad5f7ba6fedae117aa44b8d9b5aa4d389aa476
AKLOYSYS <akiloysys@gmail.com>
2022-07-05 11:37:53 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
-#include "main.h"
+#include "mani.h"
 
 
 /**
- *main - prints putchar.                                                                               
- *Return: Always 0.                                                                                                                             
- */                                                                                                                                              
-                                                                                                                                               
+ * main -prints putchar.
+ * Return: Always 0.
+ */
 
-int main(void)                                                                                                                                   
+int main(void)
 {
 _putchar('_');
 _putchar('p');
 _putchar('u');
-_putchar('t');
-_putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-Return (0) ; 
+return (0);
 }
