f1d0da1dc30f83651c8ffa16ce5085c10657a01d
AKLOYSYS <akiloysys@gmail.com>
2022-09-16 06:44:05 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,34 +1,40 @@
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints the number from 1 to 100
- * 3 multipls print Fizz instead of thr number
- * 5 multiples print Buzz instead of the number
- * 3 & 5 multiples print FizzBUzz instead of the number
- * Return: Always 0 (succcess)
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 int i;
-char f[] = "Fizz";
-char b[] = "Buzz";
-char fb[] = "FizzBuzz";
 
 for (i = 1; i <= 100; i++)
 {
-if (i == 100)
-printf("%S", b);
-else if ((i % 3 == 0) && (i % 5 == 0))
-printf("%S ", fb);
-else if (i % 3 == 0)
-printf("%S ", f);
-else if (i % 5 == 0)
-printf("%S ", b);
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
+}
+else if (i == 1)
+{
+printf("%d", i);
+}
 else
-printf("%d ", i);
+{
+printf(" %d", i);
+}
 }
 printf("\n");
-return (o);
+return (0);
 }
