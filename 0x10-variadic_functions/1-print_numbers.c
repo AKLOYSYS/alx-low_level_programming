55904e7588d8f3d03e4a96092f2df0a8c270db68
AKLOYSYS <akiloysys@gmail.com>
2022-10-13 20:29:56 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,8 +3,8 @@
 #include <stdarg.h>
 
 /**
- * print_numbers -Prints numbers, followed by a new line.
- * @seperator: The string to be printed between numbers.
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
  * @...: A variable number of numbers to be printed.
  */
@@ -20,10 +20,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 
-		if (index != (n - 1) && SEPERATOR != NULL)
-			printf("%s", seperator);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
 
 	printf("\n");
+
 	va_end(nums);
 }
